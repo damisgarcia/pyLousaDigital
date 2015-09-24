b60dbf432d883887939b690ed4bcf9dc8e847b71
damisgarcia <damisgarcia@gmail.com>
2015-09-24 15:04:32 -0300

    Integrade Glade with Python

---
+++ src/test/browser2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# encoding utf-8
+#
+# @author: Damis Garcia
+#
+
+import sys
+from gi.repository import Gtk, Gdk, WebKit
+
+class Browser2:
+    version = "0.0.6"
+    def __init__(self):
+        # Import UI
+        self.gladefile = "../../glade/browser.glade"
+        self.glade = Gtk.Builder()
+        self.glade.add_from_file(self.gladefile)
+        self.glade.connect_signals(self)
+        # Instanciando Window
+        self.win=self.glade.get_object("window1")
+        self.win.show_all()
+        self.win.maximize()
+        self.win.set_title("Lousa Digital - Version " + self.version)
+        #
+if __name__ == "__main__":
+ a = Browser2()
+ Gtk.main()
